125545826864410ee5b804bf06949e957b997a03
alexandre-lyra <alexandre.lyra1@gmail.com>
2026-04-21 22:58:14 +0000

    Texto retificado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-        return <h1>Quero lanche antes de dormir. Mê dÊ meu lanche!!</h1>
+        return <h1>Quero lanche antes de dormir. Mê dê meu lanche!! Não quero dormir com fome!</h1>
 }
 
 export default Home;
